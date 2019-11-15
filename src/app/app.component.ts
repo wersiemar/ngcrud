c61b933f78c4ab99bd36db6c48a5360343873ed1
Weronika Siedlaczek <weronika.siedlaczek@gmail.com>
2019-11-15 17:39:03 +0100

    init ng project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  title = 'crud';
+  movies = [{title: 'test'}]
+
+  constructor(private api: ApiService) {
+    this.getMovies();
+  }
+
+  getMovies = () => {
+    this.api.getAllMovies();
+  }
 }
